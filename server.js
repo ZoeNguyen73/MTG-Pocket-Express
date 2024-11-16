441a2c34de770c1b5858cc014c7e1b0ef8c1120a
Zoe Nguyen <nganguyen0703@gmail.com>
2024-11-16 13:50:17 +0800

    chore: install basic packages and set up server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// create .env file and add PORT=3000
+const port = process.env.PORT || 8800;
+
+// in .env file add CORS_ORIGIN=*
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+app.listen(port, () => {
+  console.log(`====>MTG Pocket app listening on port ${port}<====`);
+});
