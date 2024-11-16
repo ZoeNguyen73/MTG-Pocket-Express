c6bc329f39ff47d9558aead1da2cbfde23b38080
Zoe Nguyen <nganguyen0703@gmail.com>
2024-11-16 14:07:21 +0800

    chore: set up connection to MongoDB database

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
-// create .env file and add PORT=3000
 const port = process.env.PORT || 8800;
 
-// in .env file add CORS_ORIGIN=*
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
-app.listen(port, () => {
+app.listen(port, async() => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_STRING, { dbName: "MTG-Pocket" });
+  } catch (error) {
+    console.log(`====>Failed to connect to DB<==== Error: ${error}`);
+    process.exit(1);
+  }
+  console.log(`====>Connected to MongoDB`);
   console.log(`====>MTG Pocket app listening on port ${port}<====`);
-});
+})
