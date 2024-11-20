a5ea71fb3a5d7146187a8cde11af01c67aeb9ca9
Zoe Nguyen <nganguyen0703@gmail.com>
2024-11-20 11:08:10 +0800

    feat: set up basic structure for authController

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const UserModel = require("../models/userModel");
+const UserValidator = require("../validations/userValidation");
+
+const controller = {
+  register: async (req, res, next) => {
+
+  },
+
+  login: async (req, res, next) => {
+
+  },
+
+  refresh: async (req, res, next) => {
+
+  },
+
+  logout: async (req, res, next) => {
+
+  },
+};
+
+module.exports = controller;
