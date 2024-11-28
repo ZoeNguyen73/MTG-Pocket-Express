e8c64868f06130e58a0ebe0c425bb0d79b1b02da
Zoe Nguyen <nganguyen0703@gmail.com>
2024-11-28 14:43:13 +0800

    feat: add function to create corresponding UserCard upon pack opening

--- validations/userCardValidation.js
+++ validations/userCardValidation.js
@@ -1,23 +1,21 @@
 const Joi = require("Joi");
 
-const FINISHES = require("../utils/cardAttributes");
+const { FINISHES } = require("../utils/cardAttributes");
 
 const userCardValidationSchema = {
-  create: Joi.object({
+  update: Joi.object({
     user_id: Joi.string()
-    .pattern(/^[a-f\d]{24}$/i) // Matches a MongoDB ObjectId
+    .length(24)
     .required()
     .messages({
       "string.base": `"user_id" should be a valid string.`,
-      "string.pattern.base": `"user_id" must be a valid MongoDB ObjectId.`,
       "any.required": `"user_id" is a required field.`,
     }),
     card_id: Joi.string()
-      .pattern(/^[a-f\d]{24}$/i) // Matches a MongoDB ObjectId
+      .length(24)
       .required()
       .messages({
         "string.base": `"card_id" should be a valid string.`,
-        "string.pattern.base": `"card_id" must be a valid MongoDB ObjectId.`,
         "any.required": `"card_id" is a required field.`,
       }),
     finish: Joi.string()
@@ -30,25 +28,11 @@ const userCardValidationSchema = {
       }),
     quantity: Joi.number()
       .integer()
-      .min(1)
-      .required()
+      .optional()
       .messages({
         "number.base": `"quantity" must be a number.`,
         "number.integer": `"quantity" must be an integer.`,
-        "number.min": `"quantity" must be at least 1.`,
-        "any.required": `"quantity" is a required field.`,
       }),
-  }),
-  update: Joi.object({
-    quantity: Joi.number()
-    .integer()
-    .min(1)
-    .optional()
-    .messages({
-      "number.base": `"quantity" must be a number.`,
-      "number.integer": `"quantity" must be an integer.`,
-      "number.min": `"quantity" must be at least 1.`,
-    }),
   }),
 };
 
