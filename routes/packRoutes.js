358d77da237e53afa678c5e65f29552545b37863
Zoe Nguyen <nganguyen0703@gmail.com>
2024-11-28 13:32:09 +0800

    feat: add user authentication check in pack opening

--- routes/packRoutes.js
+++ routes/packRoutes.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const packController = require("../controllers/packController");
 
+const userAuth = require("../middlewares/userAuth");
+
 const router = express.Router();
 
-router.post("/open/set/:setCode/type/:packType", packController.open);
+router.post(
+  "/open/set/:setCode/type/:packType",
+  userAuth.isAuthenticated, 
+  packController.open
+);
 
 module.exports = router;
