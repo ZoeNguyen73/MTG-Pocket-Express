31a150f9b13bc23ff8ebf6d570343138083bce5c
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-03 21:02:15 +0800

    chore: add new set to SET_LIST

--- scryfall-data/seeds/setList.js
+++ scryfall-data/seeds/setList.js
@@ -8,10 +8,11 @@ const SET_LIST = [
   // "blb", // Bloomburrow
   // "mh3", // Modern Horizons 3
   // "rvr", // Ravnica Remastered
-  "lci", // The Lost Caverns of Ixalan
-  "wot", // Wilds of Eldraine: Enchanting Tales
-  "ltr", // The Lord of the Rings: Tales of Middle-earth
-  "one", // Phyrexia: All Will Be One
+  // "lci", // The Lost Caverns of Ixalan
+  // "wot", // Wilds of Eldraine: Enchanting Tales
+  // "ltr", // The Lord of the Rings: Tales of Middle-earth
+  // "one", // Phyrexia: All Will Be One
+  "clb", // commander legends: battle for Baldur's Gate
 ];
 
 module.exports = SET_LIST;
