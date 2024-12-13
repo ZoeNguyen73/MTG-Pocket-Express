e2f28f29cf57d1ab3d97673efc2ea6eab4df0d78
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-13 13:47:36 +0800

    feat: add retrieveSetBySetCode route

--- routes/setRoutes.js
+++ routes/setRoutes.js
@@ -5,3 +5,5 @@ const setController = require("../controllers/setController");
 const router = express.Router();
 
 router.get("/:setCode", setController.retrieveBySetCode);
+
+module.exports = router;
